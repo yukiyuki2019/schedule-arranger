7305d0e413db9cb45779dec11b6744abc2d5fdd1
yukiyuki2019 <yukiy0305dwycwwyhwya@gmail.com>
2020-03-02 21:24:15 +0900

    予定作成フォームを作成

---
+++ routes/schedules.js
@@ -0,0 +1,40 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
+
+router.get('/new', authenticationEnsurer, (req, res, next) => {
+  res.render('new', { user: req.user });
+});
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
+  Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255) || '（名称未設定）',
+    memo: req.body.memo,
+    createdBy: req.user.id,
+    updatedAt
+  }).then((schedule) => {
+    const candidateNames = req.body.candidates
+    .trim() // 余分な空白の削除
+    .split('\n') // 候補の分割
+    .map((s) => s.trim()) // 各候補の余分なスペースを削除
+    .filter((s) => s !== ""); // 空の候補は削除
+    const candidates = candidateNames.map((c) => {
+      return {
+        candidateName: c,
+        scheduleId: schedule.scheduleId
+      };
+    });
+    Candidate.bulkCreate(candidates).then(() => {
+      res.redirect('/schedules/' + schedule.scheduleId);
+    });
+  });
+});
+
+module.exports = router;
