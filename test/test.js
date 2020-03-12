16b11e3c95b458206d20d71ee2d5b4778bfab6ee
yukiyuki2019 <yukiy0305dwycwwyhwya@gmail.com>
2020-03-12 16:35:08 +0900

    出欠更新のテストの実装

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 'use strict';
-let request = require('supertest');
-let app = require('../app');
-let passportStub = require('passport-stub');
-let User = require('../models/user');
-let Schedule = require('../models/schedule');
-let Candidate = require('../models/candidate');
+const request = require('supertest');
+const app = require('../app');
+const passportStub = require('passport-stub');
+const User = require('../models/user');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
+const Availability = require('../models/availability');
 
 describe('/login', () => {
   before(() => {
@@ -72,25 +73,71 @@ describe('/schedules', () => {
             .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
-              if (err) return done(err);
-              // テストで作成したデータを削除
-              const scheduleId = createdSchedulePath.split('/schedules/')[1];
-              Candidate.findAll({
-                where: { scheduleId: scheduleId }
-              }).then((candidates) => {
-                const promises = candidates.map((c) => { return c.destroy(); });
-                Promise.all(promises).then(() => {
-                  Schedule.findById(scheduleId).then((s) => { 
-                    s.destroy().then(() => { 
-                      if (err) return done(err);
-                      done(); 
-                    });
-                  });
-                });
+              deleteScheduleAggregate(
+                createdSchedulePath.split('/schedules/')[1],
+                done, err
+                );
               });
-            });
         });
     });
   });
 
 });
+
+describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('出欠が更新できる', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/schedules')
+        .send({ scheduleName: 'テスト出欠更新予定1', memo: 'テスト出欠更新メモ1', candidates: 'テスト出欠更新候補1' })
+        .end((err, res) => {
+          const createdSchedulePath = res.headers.location;
+          const scheduleId = createdSchedulePath.split('/schedules/')[1];
+          Candidate.findOne({
+            where: { scheduleId: scheduleId }
+          }).then((candidate) => {
+            // 更新がされることをテスト
+            const userId = 0;
+            request(app)
+              .post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`)
+              .send({ availability: 2 }) // 出席に更新
+              .expect('{"status":"OK","availability":2}')
+              .end((err, res) => { deleteScheduleAggregate(scheduleId, done, err); });
+          });
+        });
+    });
+  });
+});
+  
+function deleteScheduleAggregate(scheduleId, done, err) {
+  Availability.findAll({
+    where: { scheduleId: scheduleId }
+  }).then((availabilities) => {
+    const promises = availabilities.map((a) => { return a.destroy(); });
+    Promise.all(promises).then(() => {
+      Candidate.findAll({
+        where: { scheduleId: scheduleId }
+      }).then((candidates) => {
+        const promises = candidates.map((c) => { return c.destroy(); });
+        Promise.all(promises).then(() => {
+          Schedule.findById(scheduleId).then((s) => { 
+            s.destroy().then(() => { 
+              if (err) return done(err);
+              done(); 
+            });
+          });
+        });
+      });
+    });
+  });
+}
